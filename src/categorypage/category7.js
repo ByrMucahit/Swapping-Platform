6af7b2b5ac34c54b472f9cd478f081dff58cebfb
ByrMucahit <m.mucahitbayar@gmail.com>
2021-01-19 00:07:45 +0300

    Categories
    Product was seperated into categories.

--- src/categorypage/category7.js
+++ src/categorypage/category7.js
@@ -16,7 +16,7 @@ firebase.auth().onAuthStateChanged((user) => {
               var host = eachOneProduct.ürün_sahibi;
               var prod = eachOneProduct.kategori;
               console.log(prod);
-              if(eachOneProduct.kategori === "Category"){
+              if(eachOneProduct.kategori === "Category6"){
                   productHtml += `<div class="col-md-6 col-sm-6"><div class="small-box-c"><div class="small-img-b"><a href="#"><img src="${eachOneProduct.link}" alt="#" /></a></div><div class="dit-t clearfix"><div class="left-ti"><p>${eachOneProduct.ürün_sahibi}</p><h4>${eachOneProduct.ürün_ismi}</h4><p><span>${eachOneProduct.kategori}</span></p></div><button id="product" value="${eachOneProduct.ürün_ismi}">"Urune Git"</button></div></div></div>`;
                   document.getElementById('catprod').innerHTML = productHtml;
               }
