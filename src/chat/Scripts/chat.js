506308b1935b77061a8e87fb34af9213a36a3886
ByrMucahit <m.mucahitbayar@gmail.com>
2021-01-25 22:16:06 +0300

    Chat
    Responsive is continues

--- src/chat/Scripts/chat.js
+++ src/chat/Scripts/chat.js
@@ -6,4 +6,19 @@ startChat.addEventListener('click',function(){
 
     document.getElementById('chatPanel').removeAttribute('style');
     document.getElementById('divStart').setAttribute('style', 'display:none');
+
+    hideChatList();
 })
+//////////////////////////////////////////////
+
+const showChatList = document.getElementById('showChatList');
+
+showChatList.addEventListener('click', function(){
+    document.getElementById('side-1').classList.remove('d-none', 'd-md-block');
+    document.getElementById('side-2').classList.add('d-none');
+})
+
+function hideChatList(){
+    document.getElementById('side-1').classList.add('d-none', 'd-md-block');
+    document.getElementById('side-2').classList.remove('d-none');
+}
