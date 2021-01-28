958f3a091074bdd7dd04774f16dde00b4b3bff4b
ByrMucahit <m.mucahitbayar@gmail.com>
2021-01-28 14:21:26 +0300

    Chat
    Adjusted something.

--- src/chat/Scripts/chat.js
+++ src/chat/Scripts/chat.js
@@ -155,3 +155,24 @@ function callback(error){
 //Call auth State Changed
 
 onFirebaseStateChanged();
+
+
+
+/* Button  */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
