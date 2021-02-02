a71048467a9fb8dc8461f1eb4770c24e390e88a8
ByrMucahit <m.mucahitbayar@gmail.com>
2021-02-02 07:50:30 +0300

    Removing
    Removing Adjusting is done

--- src/ProfilePage/delete.js
+++ src/ProfilePage/delete.js
@@ -1,21 +1,24 @@
 var closebtns = document.getElementsByClassName("close");
 var i;
-
-
  function removed(obj){
-     console.log(obj);
-     console.log(firebase.auth().currentUser.uid);
+     //console.log(obj);
+     //console.log(firebase.auth().currentUser.uid);
      var ids = obj;
      const user = firebase.auth().currentUser.uid
-     var answers =confirm(obj + "  ürününü silmek istediğinize emin misiniz ? ");
-     console.log(answers);
-     if (answers === true)
-     {
-        firebase.database().ref('users').child('products').child(user).child(ids).remove();
-        firebase.database().ref('Index').child('products').child(ids).remove();
-
-     }
-
-     
-
- }                       
+     swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            firebase.database().ref('users').child('products').child(user).child(ids).remove();
+            firebase.database().ref('Index').child('products').child(ids).remove();
+          swal(ids+ " Ürününüz silinmiştir.", {
+            icon: "success",
+          });
+        } else {
+          swal("Ürün silimi iptal edilmiştir. !!!");
+        }});}                 
