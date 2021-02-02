02c75a320acf27061d9e8e2f7e9e8c6b3c752e09
ByrMucahit <m.mucahitbayar@gmail.com>
2021-02-02 07:37:48 +0300

    Remove
    Removing Product Has been added.

--- src/ProfilePage/DisplayingProduct.js
+++ src/ProfilePage/DisplayingProduct.js
@@ -15,7 +15,9 @@ products.on("value", function(product){
             var k = keys;
             var host = eachOneProduct.ürün_sahibi;
             var prod = eachOneProduct.kategori;
-            productHtml += `<div class="col-md-4 col-sm-6"><div class="small-box-c"><div class="small-img-b"><a href="#" ><span onclick="removed()"  class="close">&times</span><img  id="s" alt="#" src="${eachOneProduct.link}" style="height:100px; height:200px;"/></a></div><div class="dit-t clearfix"><div class="left-ti"><p>${eachOneProduct.ürün_sahibi}</p><br><h4>${eachOneProduct.ürün_ismi}</h4><p><span>${eachOneProduct.kategori}</span></p></div><a href="#" tabindex= "0"><button id="product" value="${eachOneProduct.ürün_ismi}">"Urune Git"</button></a></div></div></div></div>`
+            productHtml += `<div class="col-md-4 col-sm-6"><div class="small-box-c"><div class="small-img-b"><a href="#" ><button type="button" onclick="removed(this.id)" class="close" id="${eachOneProduct.ürün_ismi}"  aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button><img  id="s" alt="#" src="${eachOneProduct.link}" style="height:100px; height:200px;"/></a></div><div class="dit-t clearfix"><div class="left-ti"><p>${eachOneProduct.ürün_sahibi}</p><br><h4>${eachOneProduct.ürün_ismi}</h4><p><span>${eachOneProduct.kategori}</span></p></div><a href="#" tabindex= "0"><button id="product" value="${eachOneProduct.ürün_ismi}">"Urune Git"</button></a></div></div></div></div>`
             
             document.getElementById('productlist').innerHTML = productHtml;
             document.getElementById('productCount').innerHTML = counter;
--- src/ProfilePage/delete.js
+++ src/ProfilePage/delete.js
@@ -2,6 +2,20 @@ var closebtns = document.getElementsByClassName("close");
 var i;
 
 
- function removed(){
+ function removed(obj){
+     console.log(obj);
+     console.log(firebase.auth().currentUser.uid);
+     var ids = obj;
+     const user = firebase.auth().currentUser.uid
+     var answers =confirm(obj + "  ürününü silmek istediğinize emin misiniz ? ");
+     console.log(answers);
+     if (answers === true)
+     {
+        firebase.database().ref('users').child('products').child(user).child(ids).remove();
+        firebase.database().ref('Index').child('products').child(ids).remove();
+
+     }
+
      
+
  }                       
